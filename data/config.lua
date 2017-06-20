d17e568dfed152a4e86863ffdb18af652132267a
DevWolf <dddd6082@hotmail.com>
2017-06-20 15:38:49 +0300

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -11,13 +11,13 @@ do local _ = {
     "addreplay",
     "deleall"
   },
-  info_text = "📌¦ Welcome My Dear\n\nTH3BOSS V14 \nFor More Information Subscribe To The Channel @llDEV1ll \n https://github.com/moody2020/TH3BOSS\n\n📌¦ Dev @TH3BOSS\n\n📌¦ Dev Bot @ll60Kllbot\n\n📌¦ Channel @llDEV1ll ",
+  info_text = "📌¦ Welcome My Dear\n\nTH3BOSS V14 \nFor More Information Subscribe To The Channel @Hlusat \n https://github.com/aliwolf/HOOK\n\n📌¦ Dev @G0vip\n\n📌¦ Dev Bot @f4f4f4bot\n\n📌¦ Channel @Hlusat ",
   moderation = {
     data = "./data/moderation.json"
   },
   sudo_users = {
-    60809019,
-      359265489,
+    103810497,
+      103810497,
   }
 }
 return _
